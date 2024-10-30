cd4eeb5d551b98c105c1e0d38f1f83dcea181c87
Ronisha Shrestha <ronisha@ronisha-Aspire-A515-57G.Home>
2024-10-30 10:54:05 +0545

    mongo setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,20 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js"
+
+dotenv.config();
 
 const app = express();
 
+app.get("/products", (req,res) => {});
+
+// console.log(process.env.MONGO_URI);1
+
+
 app.listen(5000, () => {
+    connectDB();
     console.log("Server started at http://localhost:5000");
 });
+
+
+//Ur1GIANYINuwK2Uh
