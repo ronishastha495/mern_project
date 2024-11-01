224a4941680e1855847ed17892ab531d95803f75
Ronisha Shrestha <ronisha@ronisha-Aspire-A515-57G.Home>
2024-11-01 22:43:24 +0545

    setting up postman for api request and respond

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,28 @@ dotenv.config();
 
 const app = express();
 
-app.get("/products", (req,res) => {});
+app.use(express.json());//to get the result from the postman
 
+app.post("/api/products", async (req,res) => {
+    const product = req.body;
+
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please provide all fields"});
+    }
+
+    const newProduct = new Product( product)
+
+    try { 
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct});
+    }
+    catch (error) {
+        console.error("Error in create product:", error.message);
+        res.status(500).json({ success: false, message: "Server Error " });
+    }       
+});
+
+//postman desktop app
 // console.log(process.env.MONGO_URI);1
 
 
